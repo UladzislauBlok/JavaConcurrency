34b4c5cdae4aaa4d204786c97c1eb7a471b2d200
Uladzislau Blok <blokv75@gmail.com>
2025-01-28 16:58:24 +0100

    Update doc

--- src/main/java/org/bloku/chapter1/thread/safety/locking/UnsafeAtomicProcessor.java
+++ src/main/java/org/bloku/chapter1/thread/safety/locking/UnsafeAtomicProcessor.java
@@ -7,6 +7,10 @@ import org.bloku.support.domain.NumberResponse;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
+/**
+ * When multiple variables participate in an invariant, they are not independent: the value of one constrains the allowed value(s) of the others. Thus, when updating one, you must update the others in the same atomic operation.
+ * <p>To preserve state consistency, update related state variables in a single atomic operation
+ */
 @NotThreadSafe
 public class UnsafeAtomicProcessor {
     private final AtomicInteger count = new AtomicInteger();
