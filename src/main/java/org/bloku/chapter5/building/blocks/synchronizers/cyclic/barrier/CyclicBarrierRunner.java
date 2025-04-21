9360fcf9fae295c253c09391676f51832b6185d0
Uladzislau Blok <blokv75@gmail.com>
2025-04-21 22:13:08 +0200

    Cyclic barrier example

---
+++ src/main/java/org/bloku/chapter5/building/blocks/synchronizers/cyclic/barrier/CyclicBarrierRunner.java
@@ -0,0 +1,42 @@
+package org.bloku.chapter5.building.blocks.synchronizers.cyclic.barrier;
+
+import org.bloku.support.thread.ThreadUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+import static java.lang.Thread.currentThread;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+public class CyclicBarrierRunner {
+    private static final Logger log = LoggerFactory.getLogger(CyclicBarrierRunner.class);
+    private static final Random random = new Random();
+    private static final CyclicBarrier barrier = new CyclicBarrier(2, () -> log.info("-----------------------"));
+
+    public static void main(String[] args) {
+        Thread[] threadsWithTask = ThreadUtil.createNThreadsWithTasks(2, new RandomTimeTask());
+        ThreadUtil.startThreads(threadsWithTask);
+    }
+
+    private static final class RandomTimeTask implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                for (int i = 0; i < 3; i++) {
+                    log.info("Start processing task");
+                    SECONDS.sleep(random.nextInt(5));
+                    log.info("Processing is finished");
+                    barrier.await();
+                }
+            } catch (InterruptedException e) {
+                currentThread().interrupt();
+            } catch (BrokenBarrierException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
