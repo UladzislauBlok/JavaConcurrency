3a17aead152e0c2ef422e153fe791cb83b234d51
Uladzislau Blok <blokv75@gmail.com>
2025-06-26 17:03:53 +0200

    Chapter 12. Testing Concurrent Programs

---
+++ src/main/java/org/bloku/chapter12/testing/concurrent/programs/testing/performance/PutTakeTest.java
@@ -0,0 +1,133 @@
+package org.bloku.chapter12.testing.concurrent.programs.testing.performance;
+
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * The primary extension we have to make to PutTakeTest is to measure the time taken for a run. Rather than attempting to measure the time for a single operation, we get a more accurate measure by timing the entire run and dividing by the number of operations to get a per-operation time.
+ */
+class PutTakeTest {
+    private static final ExecutorService pool = Executors.newCachedThreadPool();
+    private final AtomicInteger putSum = new AtomicInteger(0);
+    private final AtomicInteger takeSum = new AtomicInteger(0);
+    private final BarrierTimer timer;
+    private final CyclicBarrier barrier;
+    private final BoundedBuffer<Integer> bb;
+    private final int nTrials, nPairs;
+
+    /**
+     * Test it with different configuration.
+     * However, be careful about concluding from this data that you can always add more threads to a producer-consumer program that uses a bounded buffer.<br>
+     * This test is fairly artificial in how it simulates the application; the producers do al most no work to generate the item placed on the queue, and the consumers do almost no work with the item retrieved.
+     */
+    public static void main(String[] args) throws Exception {
+        int tpt = 100000; // trials per thread
+        for (int cap = 1; cap <= 1000; cap *= 10) {
+            System.out.println("Capacity: " + cap);
+            for (int pairs = 1; pairs <= 16; pairs *= 2) {
+                PutTakeTest t = new PutTakeTest(cap, pairs, tpt);
+                System.out.print("Pairs: " + pairs + "\t");
+                t.test();
+                System.out.print("\t");
+                Thread.sleep(1000);
+                t.test();
+                System.out.println();
+                Thread.sleep(1000);
+            }
+        }
+        pool.shutdown();
+    }
+
+    PutTakeTest(int capacity, int nPairs, int nTrials) {
+        this.bb = new BoundedBuffer<>(capacity);
+        this.nTrials = nTrials;
+        this.nPairs = nPairs;
+        this.timer = new BarrierTimer();
+        this.barrier = new CyclicBarrier(nPairs * 2 + 1, timer);
+    }
+
+    public void test() {
+        try {
+            timer.clear();
+            for (int i = 0; i < nPairs; i++) {
+                pool.execute(new Producer());
+                pool.execute(new Consumer());
+            }
+            barrier.await();
+            barrier.await();
+            long nsPerItem = timer.getTime() / (nPairs * (long)nTrials);
+            System.out.print("Throughput: " + nsPerItem + " ns/item");
+            assertEquals(putSum.get(), takeSum.get());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private class Producer implements Runnable {
+        public void run() {
+            try {
+                int seed = (this.hashCode() ^ (int) System.nanoTime());
+                int sum = 0;
+                barrier.await();
+                for (int i = nTrials; i > 0; --i) {
+                    bb.put(seed);
+                    sum += seed;
+                    seed = xorShift(seed);
+                }
+                putSum.getAndAdd(sum);
+                barrier.await();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private class Consumer implements Runnable {
+        public void run() {
+            try {
+                barrier.await();
+                int sum = 0;
+                for (int i = nTrials; i > 0; --i) {
+                    sum += bb.take();
+                }
+                takeSum.getAndAdd(sum);
+                barrier.await();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static class BarrierTimer implements Runnable {
+        private boolean started;
+        private long startTime, endTime;
+        public synchronized void run() {
+            long t = System.nanoTime();
+            if (!started) {
+                started = true;
+                startTime = t;
+            } else
+                endTime = t;
+        }
+        public synchronized void clear() {
+            started = false;
+        }
+        public synchronized long getTime() {
+            return endTime - startTime;
+        }
+    }
+
+    static int xorShift(int y) {
+        y ^= (y << 6);
+        y ^= (y >>> 21);
+        y ^= (y << 7);
+        return y;
+    }
+
+    // dummy
+    private void assertEquals(int i, int i1) {
+
+    }
+}
